01d532c64eb609efc6b3fb0e3cd6e6e7ac902c91
tanvirs-webpractice <71395828+tanvirs-webpractice@users.noreply.github.com>
2020-09-16 21:31:02 +0600

    Add files via upload

---
+++ HTML5/quiz1.py
@@ -0,0 +1,359 @@
+
+Week One - HTML/Web basics
+Graded Quiz • 30 min
+Due Sep 7, 12:59 PM +06
+
+Week One - HTML/Web basics
+TOTAL POINTS 30
+1.
+Question 1
+#What does HTML stand for?
+#Hyper Text Markup Language
+1 point
+Enter answer here
+2.
+Question 2
+#HTML5 tags have the same semantic meaning, regardless of the browser being used.
+
+1 point
+
+#True
+
+
+False
+
+3.
+Question 3
+#What does FTP stand for?
+# File Transfer Protocol
+
+1 point
+Enter answer here
+4.
+Question 4
+#HTTP stands for
+
+1 point
+
+#Hyper Text Transfer Protocol
+
+
+Hyper Transfer Translation Program
+
+
+Hyper Text Text Program
+
+5.
+Question 5
+#Which of the following is part of a URL?
+
+1 point
+
+#domain (or host), protocol
+
+
+protocol and doctype
+
+
+heading
+
+6.
+Question 6
+The Internet is a type of
+
+1 point
+
+Hypertext Network
+
+
+Local Area Network
+
+
+#Wide Area Network
+
+7.
+Question 7
+#What is the Request/Response Cycle?
+
+1 point
+
+The process that happens when a server requests a page and a client responds with the appropriate files
+
+
+The process that happens when a HTML5 is created with an editor
+
+
+#The process that happens when a client requests a page and a server responds with the appropriate files
+
+8.
+Question 8
+A ________________________ is a computer that is in charge of handling resource requests from multiple computers.
+# server
+1 point
+Enter answer here
+9.
+Question 9
+#Which of the following is an example of a top-level domain?
+
+1 point
+
+#.com
+
+
+umich.edu
+
+
+wikipedia.org
+
+10.
+Question 10
+#The Domain Name Service looks up the domain and returns the
+
+1 point
+
+#IP address
+
+
+server
+
+
+host
+
+
+document
+
+11.
+Question 11
+#Which organization is responsible for assigning top-level domain names?
+
+1 point
+
+Internet Engineering Task Force (IETF)
+
+
+#Internet Corporation for Assigned Numbers and Names (ICANN)
+
+
+World Wide Web Consortium (W3C)
+
+12.
+Question 12
+#The Request-Response Cycle...
+
+1 point
+
+is an outdated process used by HTML 4.1
+
+
+may require several iterations of the Request-Response Cycle.
+
+
+is performed once for each requested page.
+
+13.
+Question 13
+What does LAN stand for?
+#Local Area Network
+1 point
+Enter answer here
+14.
+Question 14
+What does WAN stand for?
+#Wide Area Network
+1 point
+Enter answer here
+15.
+Question 15
+#Which is the correct file extension for a web page?
+
+1 point
+
+.ppt
+
+
+.txt
+
+
+#.html
+
+
+.doc
+
+16.
+Question 16
+HTML uses _________ to annotate documents. This is a way of marking up the content to identify specific HTML elements.
+#tags
+1 point
+Enter answer here
+17.
+Question 17
+#Which of the following best explains the difference between syntax and semantics.
+
+1 point
+
+#A browser may have trouble displaying a page if the syntax is incorrect.
+
+
+A browser may have trouble displaying a page if the semantics is incorrect.
+
+
+Syntax and semantics are interchangeable.
+
+18.
+Question 18
+The first graphical browser was.... (please use one word answer)
+#Mosaic
+1 point
+Enter answer here
+19.
+Question 19
+#What are proprietary tags?
+
+1 point
+
+#Tags that work only on certain browsers
+
+
+Tags used to define ownership of the content
+
+
+Tags that displayed copyright information
+
+20.
+Question 20
+#Which of these tags breaks the tenet of the separation of content and layout?
+
+1 point
+
+body
+
+
+h1
+
+
+#font,
+
+21.
+Question 21
+#Which of the following are browsers?
+
+1 point
+
+Internet Explorer, Windows, Safari
+
+
+Firefox, Chrome, Safari, Sublime
+
+
+#Safari, Chrome, Internet Explorer
+
+22.
+Question 22
+#HTML5 should not...
+
+1 point
+
+be device-independent
+
+
+use markup instead of scripting where appropraite
+
+
+#take advantage of the most up-to-date plugins.
+
+23.
+Question 23
+#A basic functionality of browsers is to translate HTML documents into viewable webpages.
+
+1 point
+
+#True
+
+
+False
+
+24.
+Question 24
+#Which version of HTML introduced semantic tags?
+
+1 point
+
+HTML 3.2
+
+
+HTML 4.01
+
+
+HTML 2.0
+
+
+#HTML 5
+
+25.
+Question 25
+#The predecessor of the internet was
+
+1 point
+
+DARPA
+
+
+Al Gore
+
+
+CompuServe
+
+
+#Arpanet
+
+26.
+Question 26
+URL stands for
+
+1 point
+
+#Uniform Resource Locator
+
+
+Unordered Resource Location
+
+
+Universal Relation Locator
+
+27.
+Question 27
+CSS stands for
+
+1 point
+
+Cascading Color Scheme
+
+
+#Cascading Style Sheet
+
+
+Cascading Hypertext Style Script
+
+28.
+Question 28
+#CSS1 was introduced for public use in 1996
+
+1 point
+Enter answer here
+29.
+Question 29
+#What editor are you using?
+#Visual Studio Code
+1 point
+What do you think?
+Your answer cannot be more than 10000 characters.
+30.
+Question 30
+#What browser(s) are you using?
+#chrome,Mozila Firefox,Microsoft Edge
+1 point
+What do you think?
+Your answer cannot be more than 10000 characters.
+
+Honor Code Agreement
+I, Tanvir Anjom Siddique, understand that submitting work that isn’t my own may result in permanent failure of this course or deactivation of my Coursera account. Learn more about Coursera’s Honor Code
